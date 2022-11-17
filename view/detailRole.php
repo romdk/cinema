7b7f253cea0e6fbd0fa6f27b3f12f55e1cd0191b
romdk <drack.roman67@gmail.com>
2022-11-17 23:00:03 +0100

    detail role

---
+++ view/detailRole.php
@@ -0,0 +1,31 @@
+<?php ob_start(); 
+$role = $requete->fetch();
+$acteurs = $requete2->fetchAll();
+$films = $requete3->fetchAll();
+?>
+
+<span><?= $role['nom_personnage'] ?> a été interpreter par :
+<?php
+    foreach($acteurs as $acteur) { ?>
+        <span><?= $acteur['prenom_personne'].' '.$acteur['nom_personne'].','?></span>
+<?php } ?>
+    </span>
+<span>dans le(s) films suivants: </span>
+<table>
+    <tbody>
+        <?php
+            foreach($films as $film) { ?>
+            <tr>
+                <td><?= $film['titre'] ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+$titre = 'Details du Réalisateur';
+$titre_secondaire = 'Details du Réalisateur';
+$contenu = ob_get_clean();
+require 'view/template.php';
+?>
