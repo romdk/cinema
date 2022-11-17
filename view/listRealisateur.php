a68d6e2c7c6a2d45e22175bf1dde5629b46e01d9
romdk <drack.roman67@gmail.com>
2022-11-17 11:15:10 +0100

    listRealisateurs

---
+++ view/listRealisateur.php
@@ -0,0 +1,28 @@
+<?php ob_start() ?>
+
+<p> Il y a <?= $requete->rowCount() ?> réalisateurs</p>
+
+<table>
+    <thead>
+        <tr>
+            <th>PRENOM</th>
+            <th>NOM</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach($requete->fetchAll() as $realisateur) { ?>
+            <tr>
+                <td><?= $realisateur['prenom_personne'] ?></td>
+                <td><?= $realisateur['nom_personne'] ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+$titre = 'Liste des réalisateurs';
+$titre_secondaire = 'Liste des réalisateurs';
+$contenu = ob_get_clean();
+require 'view/template.php';
