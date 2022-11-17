3b778686d788fdcb4e69ca4d65ee2bff291acd5f
romdk <drack.roman67@gmail.com>
2022-11-17 22:37:07 +0100

    detail genre

---
+++ view/detailGenre.php
@@ -0,0 +1,24 @@
+<?php ob_start(); 
+$genre = $requete->fetch();
+$films = $requete2->fetchAll();
+?>
+
+<p>Films du genre <?= $genre['nom_genre'] ?></p>
+<table>
+    <tbody>
+        <?php
+            foreach($films as $film) { ?>
+            <tr>
+                <td><?= $film['titre'] ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+$titre = 'Details du genre';
+$titre_secondaire = 'Details du genre';
+$contenu = ob_get_clean();
+require 'view/template.php';
+?>
