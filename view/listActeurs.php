86fd01f2fea32dfbfb1f39f72c2ebf68ef0ff485
romdk <drack.roman67@gmail.com>
2022-11-18 15:57:38 +0100

    formulaire ajouter acteur

--- view/listActeurs.php
+++ view/listActeurs.php
@@ -13,6 +13,42 @@
     </tbody>
 </table>
 
+
+<h4>Ajouter un acteur</h4>
+
+<form action="index.php?action=ajoutActeur" method="post">
+    <div>
+        <label>
+            Nom:
+            <input type="text" name="nom_personne">
+        </label>
+    </div>
+    <div>
+        <label>
+            Prenom:
+            <input type="text" name="prenom_personne">
+        </label>
+    </div>
+    <div>
+        <label>
+            sexe:
+            <select name="sexe">
+                <option>Homme</option>
+                <option>Femme</option>
+            </select>
+        </label>
+    </div>
+    <div>
+        <label>
+            date:
+            <input type="date" name="date_naissance">
+        </label>
+    </div>
+    <div>
+    <input type="submit" name="ajouterActeur" value="Ajouter l'acteur" >
+    </div>
+</form>
+
 <?php
 
 $titre = 'Liste des acteurs';
