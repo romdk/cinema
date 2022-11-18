7adecbdc6ce13c4081a5d45c2440ee7993303cbf
romdk <drack.roman67@gmail.com>
2022-11-18 15:57:50 +0100

    formulaire ajouter realisateur

--- view/listRealisateur.php
+++ view/listRealisateur.php
@@ -13,6 +13,42 @@
     </tbody>
 </table>
 
+
+<h4>Ajouter un réalisateur</h4>
+
+<form action="index.php?action=ajoutRealisateur" method="post">
+    <div>
+        <label>
+            Nom:
+            <input type="text" name="nom_personne">
+        </label>
+    </div>
+    <div>
+        <label>
+            Prenom:
+            <input type="text" name="prenom_personne">
+        </label>
+    </div>
+    <div>
+        <label>
+            sexe:
+            <select name="sexe">
+                <option>Homme</option>
+                <option>Femme</option>
+            </select>
+        </label>
+    </div>
+    <div>
+        <label>
+            date:
+            <input type="date" name="date_naissance">
+        </label>
+    </div>
+    <div>
+    <input type="submit" name="ajouterRealisateur" value="Ajouter le realisateur" >
+    </div>
+</form>
+
 <?php
 
 $titre = 'Liste des réalisateurs';
