c47d0ea55eddce51e89d363319d4050984b431b9
romdk <drack.roman67@gmail.com>
2022-11-21 13:14:41 +0100

    modif template

--- view/template.php
+++ view/template.php
@@ -4,21 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="public/style.css">
     <title><?= $titre ?></title>
 </head>
 <body>
     <nav>
+    <h1><i class="fa-solid fa-ticket-simple"></i><i class="fa-solid fa-film"></i>PDO Cinéma</h1>
         <ul>
             <li><a href="index.php?action=listFilms">Films</a></li>
             <li><a href="index.php?action=listRealisateurs">Réalisateurs</a></li>
             <li><a href="index.php?action=listActeurs">Acteurs</a></li>
             <li><a href="index.php?action=listGenres">Genres</a></li>
             <li><a href="index.php?action=listRoles">Roles</a></li>
+            <li><a href="admin.php"><i class="fa-solid fa-screwdriver-wrench"></i> Admin</a></li>
         </ul>
     </nav>
     <main>
-        <div>
-            <h1>PDO Cinema</h1>
+        <div class='contenu'>
             <h2><?= $titre_secondaire ?></h2>
             <?= $contenu ?>
         </div>
