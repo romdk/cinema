79463427dcf7554a8355dd58e8cd3c66ae609884
romdk <drack.roman67@.com>
2022-11-22 15:07:37 +0100

    formulaire ajouter casting

--- admin.php
+++ admin.php
@@ -37,9 +37,9 @@
         </div>
         <div class='row'>
             <label>Genre:
-                <select name="genre[]" multiple>
-                    <option value="1">Science-fiction</option>
-                    <option value="2">Action-aventure</option>
+                <select name='genre[]' multiple>
+                    <option value='1'>Science-fiction</option>
+                    <option value='2'>Action-aventure</option>
                 </select>
             </label>
             <div>
@@ -74,7 +74,7 @@
         </form> 
     </div>
 
-    <div id='realisateurActeurRole'>
+    <div class='doubleForm'>
         <div class='adminForm'>
         <h4>Ajouter un réalisateur</h4>
 
@@ -148,50 +148,74 @@
             </div>
         </div>
             <div class='btnAjouter'>
-                <input type='submit' name='ajouterActeur' value='Ajouter l'acteur' >
+                <input type='submit' name='ajouterActeur' value="Ajouter l'acteur" >
             </div>
         </form>
         </div>
 
+    </div>
+    <div class='doubleForm'>
         <div class='adminForm'>
-        <h4>Ajouter un rôle</h4>
+            <h4>Ajouter un rôle</h4>
 
-        <form action='index.php?action=ajoutRole' method='post'>
-            <div class='row'>
+            <form action='index.php?action=ajoutRole' method='post'>
+                <div class='row'>
+                    <label>
+                        Personnage :
+                        <input type='text' name='nom_personnage'>
+                    </label>
+                </div>
+                    <div class='btnAjouter'>
+                    <input type='submit' name='ajouterRole' value='Ajouter le role' >
+                </div>
+            </form>
+        </div>
+        <div class='adminForm'>
+            <h4>Ajouter un casting</h4>
+
+            <form action='index.php?action=ajoutCasting' method='post'>
+                <div class='row'>
+                    <label>
+                        Film:
+                        <select name='film'>
+                            <option value='1'>Star Wars IV</option>
+                            <option value='2'>Blade Runner</option>
+                            <option value='3'>Star Wars V</option>
+                            <option value='4'>Indiana Jones</option>
+                            <option value='5'>Solo: A Star Wars Story</option>
+                            <option value='6'>Cry Macho</option>
+                        </select>
+                    </label>
+                </div>
+                <div class='row'>
                 <label>
-                    Personnage :
-                    <input type='text' name='nom_personnage'>
-                </label>
-            </div>
-            <div class='row'>
-                <label>
-                    Film:
-                    <select name='film'>
-                        <option value='0'>Star Wars IV</option>
-                        <option value='3'>Star Wars V</option>
-                        <option value='5'>Solo: A Star Wars Story</option>
-                        <option value='2'>Blade Runner</option>
-                        <option value='4'>Indiana Jones</option>
-                        <option value='6'>Cry Macho</option>
+                Personnage:
+                    <select name='personnage'>
+                        <option value='0'>Han Solo</option>
+                        <option value='1'>Indiana Jones</option>
+                        <option value='2'>Luke Skywalker</option>
+                        <option value='3'>Rick Deckard</option>
+                        <option value='4'>Miko</option>
+                        <option value='5'>Leia Organa</option>
                     </select>
                 </label>
-            </div>
-            <div class='row'>
-                <label>
-                Acteur:
-                <select name='acteur'>
-                    <option value='1'>Harrison Ford</option>
-                    <option value='2'>Mark Hamill</option>
-                    <option value='3'>Alden Ehrenreich</option>
-                    <option value='4'>Clint Eastwood</option>
-                    <option value='5'>Carrie Fisher</option>
-                </select>
-            </label>
-            </div>
-                <div class='btnAjouter'>
-                <input type='submit' name='ajouterRole' value='Ajouter le role' >
-            </div>
-        </form>
+                </div>
+                <div class='row'>
+                    <label>
+                        Acteur:
+                        <select name='acteur[]' multiple>
+                            <option value='1'>Harrison Ford</option>
+                            <option value='2'>Mark Hamill</option>
+                            <option value='3'>Alden Ehrenreich</option>
+                            <option value='4'>Clint Eastwood</option>
+                            <option value='5'>Carrie Fisher</option>
+                        </select>
+                    </label>
+                </div>
+                    <div class='btnAjouter'>
+                    <input type='submit' name='ajouterCasting' value='Ajouter le casting' >
+                </div>
+            </form>
         </div>
     </div>
 </main>
