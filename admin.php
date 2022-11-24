d50af2cba09043c0bc7b07ce220b07960331bf81
romdk <drack.roman67@.com>
2022-11-24 09:24:10 +0100

    ajout notes

--- admin.php
+++ admin.php
@@ -55,10 +55,15 @@
                             Note:
                             <select name='note'>
                                 <option>0</option>
+                                <option>0.5</option>
                                 <option>1</option>
+                                <option>1.5</option>
                                 <option>2</option>
+                                <option>2.5</option>
                                 <option>3</option>
+                                <option>3.5</option>
                                 <option>4</option>
+                                <option>4.5</option>
                                 <option>5</option>
                             </select>
                         </label>
